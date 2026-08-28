b1cbd2585ff08e13723823b80ef08c8f1171c384
zbear2246 <zbear@DESKTOP-T3IBP1V.localdomain>
2026-08-28 18:05:31 -0500

    learning

---
+++ src/utilities/UtilitiesSuite.java
@@ -0,0 +1,11 @@
+package utilities;
+
+import mindustry.mod.Mod;
+import mindustry.gen.Building;
+import mindustry.world.blocks.power.PowerGraph;
+import mindustry.gen.Groups;
+
+public class UtilitiesSuite extends Mod{
+
+    
+}
