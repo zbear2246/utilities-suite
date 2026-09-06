e0d4b451f55c64462c0a1425b752c66fd428b110
zbear2246 <100742502+zbear2246@users.noreply.github.com>
2026-09-06 23:00:30 +0000

    asdf

---
+++ src/utilities/features/throughputCalculator/ThroughputCalculator.java
@@ -0,0 +1,11 @@
+package utilities.features.throughputCalculator;
+
+import utilities.features.Feature;;
+
+public class ThroughputCalculator implements Feature {
+
+    @Override
+    public void init(){}
+
+    
+}
